aad62fbf393ffee8e0f9f62d076f1258a87caf4a
KyoShred <lucas.salaun2@gmail.com>
2023-09-19 16:19:10 +0200

    small fix

--- component/data.py
+++ component/data.py
@@ -3,11 +3,11 @@ import json
 
 def load_data():
     """
-  Charge les données du fichier JSON et les retourne sous forme de dictionnaire.
+    Charge les données du fichier JSON et les retourne sous forme de dictionnaire.
 
-  Returns:
-    dict: Le dictionnaire contenant les données.
-  """
+    Returns:
+      dict: Le dictionnaire contenant les données.
+    """
 
     with open("data/rdu-weather-history.json", "r") as file:
         data = json.load(file)
@@ -15,13 +15,13 @@ def load_data():
     return data
 
 
-def write_data():
+def write_data(data):
     """
-  Écrit les données dans le fichier JSON.
+    Écrit les données dans le fichier JSON.
 
-  Args:
-    data (dict): Le dictionnaire contenant les données.
-  """
+    Args:
+      data (dict): Le dictionnaire contenant les données.
+    """
 
     with open("data/rdu-weather-history.json", "w") as file:
         json.dump(data, file, indent=4)
